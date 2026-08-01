755d86fe5fbc5498b72747e44aca97d47f4b2599
Yaharuott <133761086+SH1roV12@users.noreply.github.com>
2026-08-01 17:38:54 +0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.go
@@ -0,0 +1,16 @@
+
+func groupAnagrams(strs []string) [][]string {
+	arr := make(map[string][]string)
+	for _,value := range strs{
+		byteArr := []byte(value)
+		sort.Slice(byteArr, func(i, j int) bool {return byteArr[i] < byteArr[j]})
+		key := string(byteArr)
+		arr[key] = append(arr[key], value)
+	}
+	annogrammArrays := [][]string{}
+	for _,array := range arr{
+		annogrammArrays = append(annogrammArrays,array)
+	}
+	return annogrammArrays
+}
+
