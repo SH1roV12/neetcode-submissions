ea6f2d2fd37e42b3ea808bc1e52444413e1b4e3e
Yaharuott <133761086+SH1roV12@users.noreply.github.com>
2026-08-16 16:33:20 +0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.go
@@ -0,0 +1,33 @@
+type MinStack struct {
+    stack []int
+    mins  []int
+}
+
+func Constructor() MinStack {
+    return MinStack{}
+}
+
+func (this *MinStack) Push(val int) {
+    this.stack = append(this.stack, val)
+    if len(this.mins) == 0 || val <= this.mins[len(this.mins)-1]{
+        this.mins = append(this.mins, val)
+    }
+}
+
+func (this *MinStack) Pop() {
+    if len(this.stack) == 0{
+        return
+    }
+    if this.stack[len(this.stack)-1] == this.mins[len(this.mins)-1]{
+        this.mins = this.mins[:len(this.mins)-1]
+    }
+    this.stack = this.stack[:len(this.stack)-1]
+}
+
+func (this *MinStack) Top() int {
+    return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {    
+    return this.mins[len(this.mins)-1]
+}
