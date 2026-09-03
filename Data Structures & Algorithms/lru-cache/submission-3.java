809755247305bb688c0184ea653f0c13143d1d20
Yaharuott <133761086+SH1roV12@users.noreply.github.com>
2026-09-03 23:16:51 +0700

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.java
@@ -0,0 +1,81 @@
+class LRUCache {
+        private Node older;
+        private Node newer;
+        private int cap;
+        private final HashMap<Integer, Node> map;
+         class Node{
+            int val;
+            int key;
+            private Node next;
+            private Node prev;
+            public Node(int val, int key, Node next, Node prev){
+                this.key = key;
+                this.val = val;
+                this.prev = prev;
+                this.next = next;
+            }
+             public Node(int val, int key){
+                 this.key = key;
+                 this.val = val;
+             }
+
+         }
+
+        public LRUCache(int capacity) {
+            cap = capacity;
+            map = new HashMap<>();
+            older = new Node(-1, -1);
+            newer = new Node(-1,-1);
+            older.next = newer;
+            newer.prev = older;
+        }
+        
+        void updateNewer(Node targetNode){
+            Node targetNodeNext = targetNode.next;
+            Node targetNodePrev = targetNode.prev;
+            targetNodePrev.next = targetNodeNext;
+            targetNodeNext.prev = targetNodePrev;
+            Node newerNode = newer.prev;
+            newerNode.next = targetNode;
+            targetNode.prev = newerNode;
+            targetNode.next = newer;
+            newer.prev = targetNode;
+        }
+        
+        public int get(int key) {
+            boolean exists = map.containsKey(key);
+            if (!exists){
+                return -1;
+            }
+            Node targetNode = map.get(key);
+            updateNewer(targetNode);
+            return targetNode.val;
+        }
+
+        public void put(int key, int value) {
+             if (map.containsKey(key)){
+                 Node targetNode = map.get(key);
+                 targetNode.val = value;
+                 updateNewer(targetNode);
+                 return;
+             }
+             System.out.print("Older:");
+            System.out.println(older.next.val);
+            System.out.print("Newer:");
+            System.out.println(newer.prev.val);
+            Node newNode = new Node(value, key, null, null);
+            Node newerPrev = newer.prev;
+            newerPrev.next = newNode;
+            newNode.prev = newerPrev;
+            newNode.next = newer;
+            newer.prev = newNode;
+            map.put(key, newNode);
+            if (map.size() > cap){
+                Node nodeToDelete = older.next;
+                Node nextNodeWithNodeToDelete = nodeToDelete.next;
+                older.next = nextNodeWithNodeToDelete;
+                nextNodeWithNodeToDelete.prev = older;
+                map.remove(nodeToDelete.key);
+            }
+        }
+    }
